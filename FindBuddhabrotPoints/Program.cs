05cb39314f3e7b3a727148103c2d5f432fdce0a5
David Aramant <aramant@sep.com>
2014-08-01 19:45:43 -0400

    Skeleton for program that finds interesting points

---
+++ FindBuddhabrotPoints/Program.cs
@@ -0,0 +1,64 @@
+﻿using Fractals.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FindBuddhabrotPoints
+{
+    class Program
+    {
+        private static bool _shouldStop = false;
+        private readonly static object ShouldStopLock = new object();
+
+        private static bool ShouldStop
+        {
+            get
+            {
+                lock (ShouldStopLock)
+                {
+                    return _shouldStop;
+                }
+            }
+            set
+            {
+                lock (ShouldStopLock)
+                {
+                    _shouldStop = value;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to cancel...");
+
+            Task.Factory.StartNew(() =>
+            {
+                Console.ReadKey();
+                ShouldStop = true;
+            });
+
+            Parallel.ForEach(GetRandomComplexNumbers(),
+                (number, state) =>
+                {
+                    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+                    if (ShouldStop)
+                    {
+                        state.Break();
+                    }
+                });
+        }
+
+        private static IEnumerable<Complex> GetRandomComplexNumbers()
+        {
+            var rand = new Random();
+            while (true)
+            {
+                // SO SO WRONG
+                yield return new Complex(rand.NextDouble(), rand.NextDouble());
+            }
+        }
+    }
+}
