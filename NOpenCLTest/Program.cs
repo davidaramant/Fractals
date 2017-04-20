0908533ea3024c6b5928f2a139a5585d8de33057
David Aramant <aramant@sep.com>
2017-04-20 16:50:38 -0400

    Cleaning up NOpenCL example a bit

--- NOpenCLTest/Program.cs
+++ NOpenCLTest/Program.cs
@@ -11,35 +11,9 @@ namespace NOpenCLTest
             return Platform.GetPlatforms().SelectMany(p => p.GetDevices()).Single(d => d.DeviceType == DeviceType.Gpu);
         }
 
-        private static unsafe void DoStuff()
+        private static unsafe void AddNumbers()
         {
-            var source = @"__kernel void add_numbers(
-      __global float4* data, 
-      __local float* local_result, 
-      __global float* group_result) {
-
-   float sum;
-   float4 input1, input2, sum_vector;
-   uint global_addr, local_addr;
-
-   global_addr = get_global_id(0) * 2;
-   input1 = data[global_addr];
-   input2 = data[global_addr+1];
-   sum_vector = input1 + input2;
-
-   local_addr = get_local_id(0);
-   local_result[local_addr] = sum_vector.s0 + sum_vector.s1 + 
-                              sum_vector.s2 + sum_vector.s3; 
-   barrier(CLK_LOCAL_MEM_FENCE);
-
-   if(get_local_id(0) == 0) {
-      sum = 0.0f;
-      for(int i=0; i<get_local_size(0); i++) {
-         sum += local_result[i];
-      }
-      group_result[get_group_id(0)] = sum;
-   }
-}";
+            var source = System.IO.File.ReadAllText("add_numbers.cl");
 
             using (var device = GetIntelGpu())
             using (var context = Context.Create(device))
@@ -62,7 +36,7 @@ namespace NOpenCLTest
                     fixed (float* pData = data, pSum = sum)
                     {
                         using (var inputBuffer = context.CreateBuffer(MemoryFlags.CopyHostPointer | MemoryFlags.ReadOnly, sizeof(float) * arraySize, (IntPtr)pData))
-                        using (var outputBuffer = context.CreateBuffer(MemoryFlags.CopyHostPointer | MemoryFlags.WriteOnly, sizeof(float) * numGroups, (IntPtr)pData))
+                        using (var outputBuffer = context.CreateBuffer(MemoryFlags.CopyHostPointer | MemoryFlags.WriteOnly, sizeof(float) * numGroups, (IntPtr)pSum))
                         {
                             kernel.Arguments[0].SetValue(inputBuffer);
                             kernel.Arguments[1].SetValue(size: new UIntPtr((uint)localSize * sizeof(float)), value: IntPtr.Zero);
@@ -92,7 +66,7 @@ namespace NOpenCLTest
         {
             try
             {
-                DoStuff();
+                AddNumbers();
             }
             catch (Exception e)
             {
